1c574b14a8e74e5f2f97795fcbe64ec7a8e1bc8b
Bartosz Ptaszynski <bart@yazzgoth.com>
2013-09-25 07:16:06 +0100

    correcting my_server example - this time I actually checked if it works :)

---
+++ my_server.erl
@@ -0,0 +1,59 @@
+-module(my_server).
+-export([listen/2, echo_server/1]).
+
+% defining TCP_OPTIONS macro
+-define(TCP_OPTIONS, [binary, {packet, 0}, 
+		{active, false}, {reuseaddr, true}]).
+
+start_registry() ->
+	io:format("starting registry~n"),
+	register(socket_mgr, self()),
+	registry([]).
+
+registry(Sockets) ->
+	io:format("registry listening~n"),
+	receive
+		{connected, Pid, Socket} ->
+			io:format("new connection, Pid ~p~n", [Pid]),
+			erlang:monitor(process, Pid),	
+			registry([{Pid, Socket}|Sockets]);
+		{'DOWN', _Ref, process, DeadPid, _Reason} ->
+			io:format("connection lost, DeadPid ~p~n", [DeadPid]),
+			registry(proplists:delete(DeadPid, Sockets));
+		{send, Msg} ->
+			lists:map(
+				fun({_Pid, Socket}) -> gen_tcp:send(Socket, Msg) end,
+				Sockets),
+			registry(Sockets);
+		_ -> registry(Sockets)
+	end.
+
+send_to_sockets(Msg) ->
+	io:format("Sending Msg ~p~n", [Msg]),
+	lists:map(fun(Node) -> {socket_mgr, Node} ! {send, Msg} end, nodes()).
+
+listen(Port, Handler) ->
+    {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
+    io:format("spawning registry~n"),
+    spawn(fun() -> start_registry() end),
+    io:format("starting connection accept loop~n"),
+    accept(LSocket, Handler).
+
+accept(LSocket, Handler) ->
+    {ok, Socket} = gen_tcp:accept(LSocket),
+    Pid = spawn(fun() -> Handler(Socket) end),
+    socket_mgr ! {connected, Pid, Socket},
+    accept(LSocket, Handler).
+
+echo_server(Socket) ->
+	io:format("echo server started~n"),
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+        	io:format("Data received ~p~n", [Data]),
+            send_to_sockets(Data),
+            echo_server(Socket);
+        {error, closed} ->
+        	io:format("echo connection closed"),
+            ok
+    end.
+% savvy?
